471524e36cee6aff7f6d1c6874de30128fd3eb85
Joao Neves <joaoneves792@gmail.com>
2016-07-21 19:45:27 +0100

    Working net interface selection on server

--- rcpi_server.py
+++ rcpi_server.py
@@ -1,11 +1,13 @@
 import socket
 import argparse
+from fcntl import ioctl
+from struct import pack
 from Car import Car
 from ServerState import ServerState
 from protocol import FLAGS
 
 
-IP = "127.0.0.1"
+LO = "lo"
 PORT = 5005
 MAX_PORT = 65535
 TIMEOUT = 2000
@@ -13,6 +15,7 @@ TIMEOUT = 2000
 
 def init_socket(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((ip, port))
     return sock
 
@@ -22,33 +25,9 @@ def unpack_state(data, state):
     pass
 
 
-def main():
-    parser = argparse.ArgumentParser(prog='RCPi Server', description="RCPi server (this should be executed on the RPi")
-    parser.add_argument("-p", "--port", action="store", type=int, default=PORT, choices=range(1025, MAX_PORT), help='UDP Port to listen on')
-    parser.add_argument("-t", "--timeout", action="store", type=int, default=TIMEOUT, help='Time in milliseconds to enter recovery after last status update')
-
-    args = parser.parse_args()
-
-    sock = init_socket(IP, args.port)
-
-    car = Car()
-    state = ServerState()
-
-    client_addr = await_connection(sock)
-    sock.settimeout(TIMEOUT)
-
-    while True:
-        try:
-            data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-            sock.sendto(FLAGS.get_ack(), addr)
-            print("received message:", data)
-            unpack_state(data, state)
-            car.apply_state(state)
-        except socket.timeout:
-            car.stop()  # Connection has failed enter recovery
-            sock.settimeout(0)
-            client_addr = await_connection(sock)
-            sock.settimeout(TIMEOUT)
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(ioctl(s.fileno(), 0x8915, pack('256s', ifname[:15]))[20:24])
 
 
 def await_connection(sock):
@@ -58,6 +37,44 @@ def await_connection(sock):
         if data == FLAGS.get_hello():
             sock.sendto(data, addr)
             return addr
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='RCPi Server', description="RCPi server (this should be executed on the RPi")
+    parser.add_argument("-p", "--port", action="store", type=int, default=PORT, choices=range(1025, MAX_PORT), help='UDP Port to listen on')
+    parser.add_argument("-t", "--timeout", action="store", type=int, default=TIMEOUT, help='Time in milliseconds to enter recovery after last status update')
+    parser.add_argument("-i", "--interface", action="store", type=str, default=LO, help='Network interface to bind the socket to')
+
+    args = parser.parse_args()
+
+    print("Using interface: " + args.interface)
+    ip = get_ip_address(bytes(args.interface, 'utf8'))
+    print("Listening on " + ip + ":" + str(args.port))
+
+    sock = init_socket(ip, args.port)
+
+    car = Car()
+    state = ServerState()
+
+    client_addr = await_connection(sock)
+    timeout = args.timeout/1000
+    sock.settimeout(timeout)
+
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+            sock.sendto(FLAGS.get_ack(), addr)
+            print("received message:", data)
+            unpack_state(data, state)
+            car.apply_state(state)
+        except socket.timeout:
+            print("Lost connection to client")
+            car.stop()  # Connection has failed enter recovery
+            # sock.close()
+            # sock = init_socket(ip, args.port)
+            sock.settimeout(None)
+            client_addr = await_connection(sock)
+            sock.settimeout(timeout)
 
 
 if __name__ == "__main__":
